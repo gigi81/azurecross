7303b7ae9550dc3aaac834ceb32e4b30dca4851d
Luigi Grilli <luigi.grilli@gmail.com>
2019-06-16 00:31:36 +0100

    Fixed toolchain.cmake (now builds) and

--- base/toolchain.cmake
+++ base/toolchain.cmake
@@ -11,10 +11,17 @@ set(CMAKE_Fortran_COMPILER $ENV{FC})
 
 set(CMAKE_CXX_FLAGS "-I ${cross_root}/include/")
 
+# Where to look for the target environment. (More paths can be added here)
 set(CMAKE_FIND_ROOT_PATH ${cross_root} ${cross_root}/${cross_triple})
+
+# Adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment only.
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# Search headers and libraries in the target environment only.
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_SYSROOT ${cross_root}/${cross_triple}/sysroot)
+
+set(CMAKE_INSTALL_PREFIX ${cross_root}/sysroot)
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-$ENV{TC_PROCESSOR})
