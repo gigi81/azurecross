90fb30cbaf6e16796b60486278752177b11e0f6a
Luigi Grilli <luigi.grilli@gmail.com>
2019-06-23 22:46:16 +0100

    Added all arm variants

--- base/qemu.sh
+++ base/qemu.sh
@@ -6,7 +6,7 @@ git clone https://github.com/balena-io/qemu
 cd qemu
 mkdir build
 cd build
-../configure --target-list=arm-linux-user,aarch64-linux-user,armeb-linux-user,arm-softmmu,aarch64-softmmu --prefix=/usr
+../configure --target-list=arm-linux-user,aarch64-linux-user,armeb-linux-user,aarch64_be-linux-user,arm-softmmu,aarch64-softmmu --prefix=/usr
 
 make -j8
 make install
