ae0a5c6e852ab2608e3bd771c30618e74733ea60
Luigi Grilli <luigi.grilli@gmail.com>
2019-12-23 23:24:10 +0000

    Fixed toolchain.sh to not deal with sysroot file

--- base/toolchain.sh
+++ base/toolchain.sh
@@ -9,18 +9,15 @@ export SYS_FILE=sysroot-glibc-$TC_VERSION-x86_64-$TC_TRIPLE.tar.xz
 export TC_URL=https://developer.arm.com/-/media/Files/downloads/gnu-a/$TC_VERSION/binrel/$TC_FILE
 echo "Downloading toolchain $TC_URL"
 wget -nv $TC_URL
+echo "Downloading toolchain asc $TC_URL"
 wget -nv $TC_URL.asc
+
+echo "Checking toolchain md5"
 md5sum --check $TC_FILE.asc
 
 echo "Installing toolchain into $TC_ROOT"
 mkdir $TC_ROOT
 tar -xJf $TC_FILE -C $TC_ROOT --strip 1
 
-echo "Installing sysroot into $TC_ROOT/sysroot"
-mkdir $TC_ROOT/sysroot
-tar -xJf $SYS_FILE -C $TC_ROOT/sysroot
-
 rm $TC_FILE
 rm $TC_FILE.asc
-rm $SYS_FILE
-#rm $SYS_FILE.asc
