321c3c7c6d60d965a4fc63ef93ce158d596fba88
Luigi Grilli <luigi.grilli@gmail.com>
2019-12-23 23:22:07 +0000

    Added new toolchains with 9.2 naming convetion, removed older ones

--- base/toolchain.sh
+++ base/toolchain.sh
@@ -6,16 +6,11 @@ export TC_FILE=gcc-arm-$TC_VERSION-x86_64-$TC_TRIPLE.tar.xz
 export SYS_FILE=sysroot-glibc-$TC_VERSION-x86_64-$TC_TRIPLE.tar.xz
 
 #https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-a/downloads
-echo "Downloading toolchain $TC_FILE"
-wget -nv https://developer.arm.com/-/media/Files/downloads/gnu-a/$TC_VERSION/binrel/$TC_FILE
-wget -nv https://developer.arm.com/-/media/Files/downloads/gnu-a/$TC_VERSION/binrel/$TC_FILE.asc
+export TC_URL=https://developer.arm.com/-/media/Files/downloads/gnu-a/$TC_VERSION/binrel/$TC_FILE
+echo "Downloading toolchain $TC_URL"
+wget -nv $TC_URL
+wget -nv $TC_URL.asc
 md5sum --check $TC_FILE.asc
-
-echo "Downloading sysroot $SYS_FILE"
-wget -nv https://developer.arm.com/-/media/Files/downloads/gnu-a/$TC_VERSION/binrel/sysroot/$SYS_FILE
-#asc file is not correct, skipping check
-#wget -nv https://developer.arm.com/-/media/Files/downloads/gnu-a/$TC_VERSION/binrel/sysroot/$SYS_FILE.asc
-#md5sum --check $SYS_FILE.asc
 
 echo "Installing toolchain into $TC_ROOT"
 mkdir $TC_ROOT
